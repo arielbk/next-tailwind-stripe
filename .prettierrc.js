3accbddd16d1986f3ee4cd78e92e5538f71b797b
arielbk <buchwald.kearney@gmail.com>
2021-10-15 13:07:46 +0200

    feat: add header ui

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,6 @@
 module.exports = {
   tabWidth: 2,
+  semi: false,
   printWidth: 80,
   endOfLine: 'auto',
   arrowParens: 'avoid',
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-purple': '#5267df',
+        'custom-red': '#fa5959',
+        'custom-blue': '#242a45',
+        'custom-grey': '#9194a2',
+        'custom-white': '#f7f7f7',
+      },
+    },
+
+    fontFamily: {
+      Poppins: ['Poppins', 'sans-serif'],
+    },
+    container: {
+      center: true,
+      padding: '1rem',
+      screens: {
+        lg: '1124px',
+        xl: '1124px',
+        '2xl': '1124px',
+      },
+    },
   },
   variants: {
     extend: {},
